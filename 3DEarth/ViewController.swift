7cfddef0d1ebfc7d291442f98006489328d6b728
Patricia <patysiq@yahoo.com.br>
2020-09-16 17:39:17 -0300

    Initial Commit

---
+++ 3DEarth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3DEarth
+//
+//  Created by PATRICIA S SIQUEIRA on 16/09/20.
+//  Copyright © 2020 PATRICIA S SIQUEIRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
